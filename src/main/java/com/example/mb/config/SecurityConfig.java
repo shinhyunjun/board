05a324470c77ad3039d99e00fe18a7ddfb3f078c
shj <mae03170@naver.com>
2020-09-24 17:34:20 +0900

    성공

--- src/main/java/com/example/mb/config/SecurityConfig.java
+++ src/main/java/com/example/mb/config/SecurityConfig.java
@@ -4,17 +4,25 @@ import com.example.mb.common.security.CustomAccessDeniedHandler;
 import com.example.mb.common.security.CustomLoginSuccessHandler;
 import lombok.extern.java.Log;
 import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.sql.DataSource;
 
 @Log
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
@@ -46,11 +54,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
 
-        //지정된 아이디와 패스워드로 로그인이 가능하도록 설정한다.(밑에 두 username이 같으면 안됨)
-        auth.inMemoryAuthentication().withUser("member").password("{noop}1234").roles("MEMBER"); //회원
 
-        auth.inMemoryAuthentication().withUser("admin").password("{noop}1234").roles("ADMIN");  //관리자
+        // 스프링 시큐리티가 원하는 결과를 반환하는 쿼리를 작성한다.
+        String query1 = "SELECT user_id , user_pw , enabled FROM member2 WHERE user_id = ?";
+        String query2 ="SELECT b.user_id, a.auth FROM member_auth a, member2 b WHERE a.user_no = b.user_no AND b.user_id = ?";
 
+
+
+
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+
+                .usersByUsernameQuery(query1)
+                .authoritiesByUsernameQuery(query2)   //작성한 쿼리를 지정한다.
+
+                .passwordEncoder(createPasswordEncoder()); //비밀번호 암호와 처리기를 지정한다.
     }
 
 
@@ -66,6 +84,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public AuthenticationSuccessHandler createAuthenticationSuccessHandler(){
         return new CustomLoginSuccessHandler();
+    }
+
+
+    @Bean
+    public PasswordEncoder createPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
